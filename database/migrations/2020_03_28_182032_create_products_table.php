6165c5675baa6a205b2f43fe3f6f8373e1eed521
Jason <jasonmoramora@outlook.com>
2020-03-28 18:43:08 -0500

    Creacion de modelado de base de datos

---
+++ database/migrations/2020_03_28_182032_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id');	
+
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->float('precio', 8, 2);
+            $table->smallInteger('stock');
+            $table->string('imagen');	
+            $table->boolean('estado');
+
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2020_03_28_195800_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->foreignId('user_id');
+            $table->string('tipo_comprobante');
+            $table->string('num_comprobante');
+            $table->string('serie_comprobante');            
+            $table->timestamps();
+            $table->tinyInteger('impuesto');
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('user_id')->references('id')->on('users');            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
